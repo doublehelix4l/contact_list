b6c8202e7bdfc2b48c4a639a175647ccbbeb4ffd
doublehelix4l <jakercrockett@gmail.com>
2023-06-05 08:04:49 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+import {ContactList} from 'react';
+//const [contacts, setContacts] = useState(dummyContacts)
+
+export default function App() {
+  return (
+    <>
+      <ContactList />
+    </>
+  );
+}
